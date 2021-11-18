93b5bcb45584dc4e20bc11f7fa64da22be2e2f86
budisandiana <61983236+budisandiana@users.noreply.github.com>
2021-11-18 17:53:57 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/components/login/login.component';
+import { NavbarComponent } from './pages/components/navbar/navbar.component';
+import { FeedbackPageComponent } from './pages/components/feedback-page/feedback-page.component';
+import { FeedbackListComponent } from './pages/components/feedback-list/feedback-list.component';
+import { RequestFeedbackComponent } from './pages/components/request-feedback/request-feedback.component';
+import {LoginModule} from "./login/login.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FeedbackPageComponent,
+    FeedbackListComponent,
+    RequestFeedbackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
